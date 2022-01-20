003f694c1a35326a8f5602d2a2581586ccd922aa
rhkearns <ryankearns217@gmail.com>
2022-01-20 15:43:20 -0500

    api call function

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import MovieListing from '../movieListing/MovieListing';
+import movieApi from '../../common/apis/movieApi';
+import { APIKey } from '../../common/apis/movieApiKey';
 
 const Home = () => {
-  return (
-    <div>
-      Home
-    </div>
-  );
+	useEffect(() => {
+		const movieText = 'Harry';
+		const fetchMovies = async () => {
+			const response = await movieApi.get(
+				`?apiKey=${APIKey}&s=${movieText}&type=movie`
+			)
+      .catch((err) => {
+        console.log("Err:", err);
+      })
+      console.log('API RESPONSE', response);
+		};
+    fetchMovies()
+	}, []);
+
+	return (
+		<div>
+			<div className="banner-img"></div>
+			<MovieListing />
+		</div>
+	);
 };
 
 export default Home;
