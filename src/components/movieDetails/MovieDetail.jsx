4f46d880a0761737a0b161aee3d58e909b9c54cb
rhkearns <ryankearns217@gmail.com>
2022-01-21 13:23:07 -0500

    add conditional loading message

--- src/components/movieDetails/MovieDetail.jsx
+++ src/components/movieDetails/MovieDetail.jsx
@@ -24,49 +24,56 @@ const MovieDetail = () => {
 
 	return (
 		<div className="movie-section">
-			<div className="section-left">
-				<div className="movie-title">{data.title}</div>
-				<div className="movie-rating">
-					<span>
-						IMDB Rating <i className="fa fa-star"></i> : {data.imdbRating}
-					</span>
-					<span>
-						IMDB Votes <i className="fa fa-thumbs-up"></i> : {data.imdbVotes}
-					</span>
-					<span>
-						Runtime <i className="fa fa-film"></i> : {data.Runtime}
-					</span>
-					<span>
-						Year <i className="fa fa-calendar"></i> : {data.Year}
-					</span>
-				</div>
-				<div className="movie-plot">{data.Plot}</div>
-				<div className="movie-info">
-					<div>
-						<span>Director</span>
-						<span>{data.Director}</span>
+			{Object.keys(data).length === 0 ? (
+				<div>...Loading</div>
+			) : (
+				<>
+					<div className="section-left">
+						<div className="movie-title">{data.title}</div>
+						<div className="movie-rating">
+							<span>
+								IMDB Rating <i className="fa fa-star"></i> : {data.imdbRating}
+							</span>
+							<span>
+								IMDB Votes <i className="fa fa-thumbs-up"></i> :{' '}
+								{data.imdbVotes}
+							</span>
+							<span>
+								Runtime <i className="fa fa-film"></i> : {data.Runtime}
+							</span>
+							<span>
+								Year <i className="fa fa-calendar"></i> : {data.Year}
+							</span>
+						</div>
+						<div className="movie-plot">{data.Plot}</div>
+						<div className="movie-info">
+							<div>
+								<span>Director</span>
+								<span>{data.Director}</span>
+							</div>
+							<div>
+								<span>Stars</span>
+								<span>{data.Actors}</span>
+							</div>
+							<div>
+								<span>Genres</span>
+								<span>{data.Genre}</span>
+							</div>
+							<div>
+								<span>Languages</span>
+								<span>{data.Languages}</span>
+							</div>
+							<div>
+								<span>Awards</span>
+								<span>{data.Awards}</span>
+							</div>
+						</div>
 					</div>
-					<div>
-						<span>Stars</span>
-						<span>{data.Actors}</span>
+					<div className="section-right">
+						<img src={data.Poster} alt={data.Title} />
 					</div>
-					<div>
-						<span>Genres</span>
-						<span>{data.Genre}</span>
-					</div>
-					<div>
-						<span>Languages</span>
-						<span>{data.Languages}</span>
-					</div>
-					<div>
-						<span>Awards</span>
-						<span>{data.Awards}</span>
-					</div>
-				</div>
-			</div>
-			<div className="section-right">
-				<img src={data.Poster} alt={data.Title} />
-			</div>
+				</>
+			)}
 		</div>
 	);
 };
