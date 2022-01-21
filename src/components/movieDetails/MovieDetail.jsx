5c0a0b7e511fb968c5f655468c0911e2ab344b1d
rhkearns <ryankearns217@gmail.com>
2022-01-21 12:58:26 -0500

    fetching data correctly

--- src/components/movieDetails/MovieDetail.jsx
+++ src/components/movieDetails/MovieDetail.jsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router';
+import { useDispatch } from 'react-redux';
+import {fetchAsyncMovieOrShowDetail, getSelectedMovieOrShow} from '../../features/movies/movieSlice'
+import { useSelector } from 'react-redux';
 
 const MovieDetail = () => {
+  const {imdbID} = useParams()
+  const dispatch = useDispatch()
+  const data = useSelector(getSelectedMovieOrShow)
+  console.log(data);
+
+  useEffect(() => {
+    dispatch(fetchAsyncMovieOrShowDetail(imdbID))
+  }, [dispatch, imdbID])
+
   return (
     <div>
       Movie Details
