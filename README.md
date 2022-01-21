72505e5ddd0eb30a87fe72a1978ac60cf6e9fc80
rhkearns <ryankearns217@gmail.com>
2022-01-21 14:52:08 -0500

    add links to readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Tutorial from https://www.youtube.com/watch?v=EnIRyNT2PMI
+Tutorial from - 
+Part 1: https://www.youtube.com/watch?v=EnIRyNT2PMI
+Part 2: https://www.youtube.com/watch?v=FeYTMsWpgQo
+Part 3: https://www.youtube.com/watch?v=VEp9dW0MeDA
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -13,6 +13,9 @@ const Header = () => {
 	const dispatch = useDispatch();
 	const submitHandler = e => {
 		e.preventDefault();
+    if(term === '') {
+      return alert ("Please enter search term")
+    }
 		dispatch(fetchAsyncMovies(term));
 		dispatch(fetchAsyncShows(term));
     setTerm('')
