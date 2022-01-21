470550cdfe7da2cb064e2ed14d7e0437205371f9
rhkearns <ryankearns217@gmail.com>
2022-01-21 12:45:10 -0500

    add fetch async, add fetch shows, add shows to listing

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,25 +1,18 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import MovieListing from '../movieListing/MovieListing';
-import movieApi from '../../common/apis/movieApi';
-import { APIKey } from '../../common/apis/movieApiKey';
-import { addMovies } from '../../features/movies/movieSlice';
+import {
+	fetchAsyncMovies,
+	fetchAsyncShows,
+} from '../../features/movies/movieSlice';
 
 const Home = () => {
 	const dispatch = useDispatch();
-  const movieText = 'Harry';
-	
-  useEffect(() => {
-		const fetchMovies = async () => {
-			const response = await movieApi
-				.get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
-				.catch(err => {
-					console.log('Err:', err);
-				});
-			dispatch(addMovies(response.data));
-		};
-		fetchMovies();
-	}, []);
+
+	useEffect(() => {
+		dispatch(fetchAsyncMovies());
+		dispatch(fetchAsyncShows());
+	}, [dispatch]);
 
 	return (
 		<div>
--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -1,7 +1,40 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import movieApi from '../../common/apis/movieApi';
+import { APIKey } from '../../common/apis/movieApiKey';
+
+export const fetchAsyncMovies = createAsyncThunk(
+	'movies/fetchAsyncMovies',
+	async () => {
+		try {
+			const movieText = 'Harry';
+			const response = await movieApi.get(
+				`?apiKey=${APIKey}&s=${movieText}&type=movie`
+			);
+			return response.data;
+		} catch (error) {
+			throw error;
+		}
+	}
+);
+
+export const fetchAsyncShows = createAsyncThunk(
+	'movies/fetchAsyncShows',
+	async () => {
+		try {
+			const seriesText = 'Friends';
+			const response = await movieApi.get(
+				`?apiKey=${APIKey}&s=${seriesText}&type=series`
+			);
+			return response.data;
+		} catch (error) {
+			throw error;
+		}
+	}
+);
 
 const initialState = {
 	movies: {},
+	shows: {}
 };
 
 const movieSlice = createSlice({
@@ -12,8 +45,25 @@ const movieSlice = createSlice({
 			state.movies = payload;
 		},
 	},
+	extraReducers: {
+		[fetchAsyncMovies.pending]: () => {
+			console.log('pending');
+		},
+		[fetchAsyncMovies.fulfilled]: (state, { payload }) => {
+			console.log('fetched sucessfully');
+			return {...state, movies: payload}
+		},
+		[fetchAsyncMovies.rejected] : () => {
+			console.log('rejected');
+		},
+		[fetchAsyncShows.fulfilled]: (state, { payload }) => {
+			console.log('fetched sucessfully');
+			return {...state, shows: payload}
+		},
+	},
 });
 
 export const { addMovies } = movieSlice.actions;
 export const getAllMovies = state => state.movies.movies;
+export const getAllShows = state => state.movies.shows;
 export default movieSlice.reducer;
